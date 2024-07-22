b7a54e7f6c7ce7ef20c5acf212c466b7cb84b252
Jominsu <whalstn9@hecto.co.kr>
2024-07-22 11:17:09 +0900

    Revert "[ALL] esbuild-plugin-babel-next 플러그인 적용"
    This reverts commit 7ea850d1f8b8d6a6e3aa0fb1a41d37f7d4cd377f.

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-import withEsbuild from "esbuild-plugin-babel-next";
 /** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-// Use:
-export default withEsbuild({
-  esbuild: {
-    // esbuild 옵션
-    loader: "tsx", // TypeScript 및 JSX 지원
-    target: "es2015", // 대상 ECMAScript 버전
-    minify: true, // 프로덕션 빌드시 코드 최소화
-    drop_annotations: true, // 주석 제거
-    dro_debugger: true, // 디버거 문 제거
-  },
-  // 기타 Next.js 옵션
-  webpack: (config, { dev, isServer }) => {
-    // 추가적인 webpack 설정
-    return config;
-  },
-});
+export default nextConfig;
