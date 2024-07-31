579314e2a95050a94839a315ef10f980701bb08c
Jominsu <whalstn9@gmail.com>
2024-07-31 17:01:27 +0900

    [Setting] turbo 적용 및 express 설치  - yarn dev명령어로 서버 띄우기 완료  - server/src 변경사항이 git에 인식되지 않아서 수정 필요함.

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/server/:path*",
+        destination: "http://localhost:3001/api/:path*", // Nest.js 서버 주소
+      },
+    ];
+  },
+};
 
 export default nextConfig;
